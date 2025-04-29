6a9b976d1494ddf4eaa261d9da2ff0a98e53553e
divyanshu967 <divyanshugombar@gmail.com>
2025-04-29 22:17:49 +0530

    chat bot

--- dashboard/employee_dashboard_form.php
+++ dashboard/employee_dashboard_form.php
@@ -104,6 +104,18 @@ $username = $_SESSION['name'];
         <!-- Logout Button -->
         <button class="logout-btn" onclick="window.location.href='../login/login_form.php'">Logout</button>
     </div>
+<!-- Chatbot Widget (Tawk.to) -->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"), s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/YOUR_PROPERTY_ID/1hxxx'; // Replace YOUR_PROPERTY_ID with your actual Tawk.to ID
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
 
 </body>
 </html>
